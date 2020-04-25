b699b42f3e0128e32a1e721918702d4d50380adb
Naveen Kashyap <naveen@kemuri.in>
2020-04-25 16:52:17 +0530

    Update news.vue

--- src/components/news.vue
+++ src/components/news.vue
@@ -20,8 +20,8 @@ export default {
     data(){
         return{
              datahead:[{h1:'Latest News',p1:'Be updated with latest news for exciting ',p2:'prizes,coupons and many more!'}],
-             newsdata:[{img:'https://react-next-landing.redq.io/_next/static/images/blog-1-5700c118c247382c19adecc89d3b9949.svg',head:'Built for drivers, with drivers',info:'The new Driver app helps you earn smarter and supports you–like a partner–at every turn.'},
-             {img:'https://react-next-landing.redq.io/_next/static/images/blog-2-6d408e493422840dc6983501beb22797.svg',head:'Bike Share',info:'All the Riders have been verified by us. Not random people with bikes that we don’t know.'}]
+             newsdata:[{img:'https://react-next-landing.redq.io/_next/static/images/blog-1-e11af3e49789bb872a39929426a8539c.svg',head:'Built for drivers, with drivers',info:'The new Driver app helps you earn smarter and supports you–like a partner–at every turn.'},
+             {img:'https://react-next-landing.redq.io/_next/static/images/blog-2-128f48d6828ff7a383a50996c181eb41.svg',head:'Bike Share',info:'All the Riders have been verified by us. Not random people with bikes that we don’t know.'}]
         }
     }
    
